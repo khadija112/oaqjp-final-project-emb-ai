4fcff97d36f9e26a6abce28ebc202cc85035f439
soneya <soneya@Mac.lan>
2026-04-17 16:14:57 -0400

    initial commit

---
+++ emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    payload = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print("STATUS CODE:", response.status_code)  # DEBUG (important)
+
+    data = response.json()
+
+    print("RAW RESPONSE:", data)  # DEBUG (important)
+
+    emotions = data['emotionPredictions'][0]['emotion']
+
+    dominant = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant
+    }
